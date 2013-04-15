008665938ab4b063b4a6ded8a33359a0c318c89d
JakeEllis <jmelli12@gmail.com>
2013-04-14 20:27:09 -0400

    Added database EDMX and created personTEST unit test - getting error when adding person to DB table because of FK constraint in cooresponding tables

---
+++ Playlister/Playlister.dal/PlaylisterDEV.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Jake Ellis\Playlister\Feux11\Playlister\Playlister.dal\PlaylisterDEV.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
---
+++ Playlister/Playlister.dal/Repositories/PersonRepo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Playlister.dal;
+
+
+namespace Playlister.dal.Repositories
+{
+
+    public class PersonRepo : IRepository<Person>
+    {
+        private PlaylisterDEV _context = null;
+
+        public PersonRepo()
+        {
+            _context = new PlaylisterDEV();
+        }
+
+        public Person getById(Person playlisterObject)
+        {
+            return _context.People.Find(playlisterObject.Person_ID);
+         
+        }
+
+        public Person[] getAll()
+        {
+            return _context.People.ToArray();
+        }
+
+        public void add(Person playlisterObject)
+        {
+            _context.People.Add(playlisterObject);
+            _context.SaveChanges();
+        }
+
+        public void update(Person playlisterObject)
+        {
+            _context.Entry(playlisterObject).State = System.Data.EntityState.Modified;
+            _context.SaveChanges();
+
+        }
+
+        public void remove(Person playlisterObject)
+        {
+            _context.People.Remove(playlisterObject);
+            _context.SaveChanges();
+        }
+
+        public IQueryable<Person> query(System.Linq.Expressions.Expression<Func<Person, bool>> filter)
+        {
+            return _context.People.Where(filter);
+        }
+    }
+}
